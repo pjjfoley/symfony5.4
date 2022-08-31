7346c92c9f20cd21a939bfe48cbbfea42dc63f25
Paul Foley <pjjfoley@hotmail.com>
2022-08-31 12:12:32 +0100

    v1.0.1 - Add Carbon package and use ISO to test on deployed environment.

---
+++ src/Controller/TestController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Carbon\Carbon;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/test")
+ */
+class TestController extends AbstractController
+{
+    const DAYS_BEFORE_START_DATE = 30;
+
+    /**
+     * @Route("/carbon_iso", name="test_carbon_iso")
+     * @return JsonResponse
+     */
+    public function carbonISO(): JsonResponse
+    {
+        $defaultStartDate = Carbon::now()->subDays(self::DAYS_BEFORE_START_DATE)->firstOfMonth();
+        $startDate = $defaultStartDate->isoFormat('DD-MM-YYYY');
+
+        return $this->json([
+            'start_date' => $startDate,
+        ]);
+    }
+}
